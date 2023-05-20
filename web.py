7974a7bf283703d7a266e7e574c83fdc83a76498
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2023-05-21 00:58:49 +0300

    commiting test files

--- web.py
+++ web.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+##
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
+data=data = [
+        {"number": "123", "name": "John"},
+        {"number": "456", "name": "Jane"}
+    ]
+##
+@app.get('/')
+def get_welcome(request: Request):
+    return templates.TemplateResponse("welcome.html", {"request": request})
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post('/')
+async def post_welcome(request: Request):
+    form = await request.form()
+    number = form.get("number")
+    name = form.get("name")
+    # Store the form data in a dictionary or JSON file
+    data.append({"number":number,"name":name})
+    return templates.TemplateResponse("welcome.html", {"request": request})
+
+@app.get('/data')
+async def get_data(request: Request):
+    # Retrieve the data from the dictionary or JSON file
+    
+    return templates.TemplateResponse("data.html", {"request": request, "data": data})
